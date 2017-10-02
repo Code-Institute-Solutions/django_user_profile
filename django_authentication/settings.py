88dee01a4c0f33436efd64ce88e0373a5394b525
Richard Dalton <richard@codeinstitute.net>
2017-10-02 00:18:43 +0000

    Can sign in using email address or username

--- django_authentication/settings.py
+++ django_authentication/settings.py
@@ -101,6 +101,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'accounts.backends.EmailAuth',
+]
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
